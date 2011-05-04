c7e3f82d6042ce4d4fbbb72402a1dd735cbcb0f8
fedefrappi <federico.frappi@gmail.com>
2011-05-04 23:07:15 +0200

    Added Chapter List PopOver

---
+++ AePubReader/Classes/ChapterListViewController.h
@@ -0,0 +1,16 @@
+//
+//  ChapterListViewController.h
+//  AePubReader
+//
+//  Created by Federico Frappi on 04/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ChapterListViewController : UITableViewController {
+    
+}
+
+@end
